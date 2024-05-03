4814544716b018e39c6f562644dd627dc7fa9771
Matt Mihic <matt.mihic@gmail.com>
2024-05-03 14:07:16 -0400

    Require Value to fmt.Stringer

--- src/pkg/sheets/values_test.go
+++ src/pkg/sheets/values_test.go
@@ -1,11 +1,13 @@
 package sheets
 
 import (
+	"context"
 	"testing"
 	"time"
 
 	"github.com/mmihic/golib/src/pkg/timex"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestStringToValue(t *testing.T) {
@@ -31,5 +33,47 @@ func TestStringToValue(t *testing.T) {
 			actual := StringToValue(tt.input)
 			assert.Equal(t, tt.expected, actual)
 		})
+	}
+}
+
+func TestSliceValueIter(t *testing.T) {
+	iter := SliceValueIter([]Value{
+		StringValue("A"),
+		StringValue("B"),
+		StringValue("C"),
+		StringValue("D"),
+	})
+	require.Equal(t, 4, iter.Len())
+
+	var (
+		indexes []int
+		values  []string
+	)
+
+	for iter.Next(context.TODO()) {
+		require.NoError(t, iter.Err())
+
+		indexes = append(indexes, iter.Index())
+		values = append(values, iter.Value().String())
+	}
+
+	require.NoError(t, iter.Err())
+	require.Equal(t, 4, iter.Len())
+	require.Equal(t, []int{0, 1, 2, 3}, indexes)
+	require.Equal(t, []string{"A", "B", "C", "D"}, values)
+}
+
+func TestSingleValueIter(t *testing.T) {
+	iter := SingleValueIter(StringValue("Foo"))
+	require.Equal(t, 1, iter.Len())
+	require.True(t, iter.Next(context.TODO()))
+	require.Equal(t, 0, iter.Index())
+	require.Equal(t, StringValue("Foo"), iter.Value())
+	require.NoError(t, iter.Err())
+
+	for i := 0; i < 10; i++ {
+		require.False(t, iter.Next(context.TODO()))
+		require.NoError(t, iter.Err())
+		require.Equal(t, 1, iter.Len())
 	}
 }
